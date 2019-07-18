56c6cab3107d6d02a4d80a68f02054ee69e43dc7
Michelle Chow <40366174+michellechowdc@users.noreply.github.com>
2019-07-18 15:56:39 -0700

    Update testFinalProj.js

--- testFinalProj.js
+++ testFinalProj.js
@@ -1,4 +1,23 @@
-// Get and display movie info
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.contextMenus.create({
+//     "id": "sampleContextMenu",
+//     "title": "Sample Context Menu",
+//     "contexts": ["selection"]
+//   });
+// });
+
+// function searchMovieAPI()
+// {
+//  makeMovieRequest();
+//  chrome.tabs.create({url:   "https://www.myapifilms.com/imdb/idIMDB?title=" + movieName + "&token=b195f179-9b26-43b2-9d5d-862f50c35050&format=json&language=en-us&trailers=1&filmingLocations=1"})
+//  searchMovieAPI();
+// }
+//
+// chrome.contextMenus.create({title: "Search Movie API", contexts:["selection"], onclick: searchMovieAPI});
+
+
+//
+// // Get and display movie info
 function makeMovieRequest() {
 var movieName = document.getElementById("movie-name").value;
 
@@ -7,8 +26,10 @@ if (movieName === "") {
   return;
 }
 
+// HERE
+movieName = "Finding Nemo"
 var query =
-  "https:/www.myapifilms.com/imdb/idIMDB?title=" + movieName + "&token=b195f179-9b26-43b2-9d5d-862f50c35050&format=json&language=en-us&trailers=1&filmingLocations=1"
+  "https://www.myapifilms.com/imdb/idIMDB?title=" + movieName + "&token=b195f179-9b26-43b2-9d5d-862f50c35050&format=json&language=en-us&trailers=1&filmingLocations=1"
 query = query.replace(/ /g, "+");
 
 request = new XMLHttpRequest();
@@ -33,85 +54,58 @@ document.getElementById("showPoster").src = movieInfo.urlPoster;
 
 document.getElementById("showSummary").innerHTML = movieInfo.simplePlot;
 
-// document.getElementById("showTrailer").className = "show";
+// document.getElementById("showTrailer").setAttribute = "hidden";
+// document.getElementById("showTrailer").classList.replace(hide, show);
 document.getElementById("showTrailer").src = movieInfo.trailer.qualities[0].videoURL;
 
-if (movieInfo.filmingLocations[0].location === "") { document.getElementById("showLocation").innerHTML = "No location found";
-} else {
+// if (movieInfo.filmingLocations[0].location === "") { document.getElementById("showLocation").innerHTML = "No location found";
+// } else {
 document.getElementById("showLocation").innerHTML = movieInfo.filmingLocations[0].location;
-}
+// }
 
-openCordData();
+// document.getElementById("movie-name").setAttribute("", "hide");
+// document.getElementById("movieButton").setAttribute("", "hide");
+openMap();
 }
 
 
 
 
 // get map coordinates
-function openCordData() {
+function openMap() {
 var movieAddress = document.getElementById("showLocation").innerHTML;
 
-if (movieAddress === "No location found") {
-// show error on map
-document.getElementById("showCords").innerHTML = "Map Unavailable";
+if (movieAddress === "q") {
+  document.getElementById("googleMap").innerHTML = "Map Unavailable";
 } else {
-// need to delete name of location first
-movieAddress = movieAddress.split("- ").pop();
+  // movieAddress = movieAddress.split("- ").pop();
 
-var addQuery =
-  "https://www.mapquestapi.com/geocoding/v1/address?key=SJ4ZCUYnAgW7cXTqRSonL65wis7pNme7&inFormat=kvp&outFormat=json&location=" + movieAddress + "&thumbMaps=false"
+  // var addQuery =
+  //   "https://www.mapquestapi.com/geocoding/v1/address?key=SJ4ZCUYnAgW7cXTqRSonL65wis7pNme7&inFormat=kvp&outFormat=json&location=" + movieAddress + "&thumbMaps=false"
 
-addQuery = addQuery.replace(/ /g, "+");
-addQuery = addQuery.replace(/, /g, "%2C");
+  // addQuery = addQuery.replace(/ /g, "+");
+  // addQuery = addQuery.replace(/, /g, "%2C");
+  movieAddress = movieAddress.replace(/ - /g, ",");
+  movieAddress = movieAddress.replace(/, /g, ",");
+  movieAddress = movieAddress.replace(/ /g, "+");
+  // document.getElementById("showCords").innerHTML = movieAddress;
 
-request = new XMLHttpRequest();
-request.open("GET", addQuery, true);
-request.onload = processCoords;
-request.send();
+document.getElementById("googleMap").src = "https://www.google.com/maps/embed/v1/place?q=" + movieAddress +  "&key=AIzaSyCHVB2GQRul-sTvAW8vzB0RFjJ5J7-90hY";
+
+  // request = new XMLHttpRequest();
+  // request.open("GET", addQuery, true);
+  // request.onload = processCoords;
+  // request.send();
+  }
 }
-}
-
-function processCoords() {
-var mapData = JSON.parse(request.responseText);
-var addCords = mapData.results[0].locations[0].latLng;
-
-var addLat = addCords.lat;
-var addLng = addCords.lng;
-
-// document.getElementById("showCords").innerHTML = addLat + ", " + addLng;
-
-// L.mapquest.key = 'SJ4ZCUYnAgW7cXTqRSonL65wis7pNme7';
 //
-// var map = L.mapquest.map('map', {
-//   center: [addLat, addLng],
-//   layers: L.mapquest.tileLayer('map'),
-//   zoom: 12
-// });
+// function processCoords() {
+// var mapData = JSON.parse(request.responseText);
+// var addCords = mapData.results[0].locations[0].latLng;
 //
-// map.addControl(L.mapquest.control());
-
-
-var mymap = L.map('map').setView([addLat, addLng], 13);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'SJ4ZCUYnAgW7cXTqRSonL65wis7pNme7'
-}).addTo(mymap);
-
-}
-
-
-
-// make map
-function openMap() {
-        // L.mapquest.key = 'SJ4ZCUYnAgW7cXTqRSonL65wis7pNme7';
-        //
-        // var map = L.mapquest.map('map', {
-        //   center: [addLat, addLng],
-        //   layers: L.mapquest.tileLayer('map'),
-        //   zoom: 12
-        // });
-        //
-        // map.addControl(L.mapquest.control());
-      }
+// var addLat = addCords.lat;
+// var addLng = addCords.lng;
+//
+// // document.getElementById("showCords").innerHTML = addLat + ", " + addLng;
+// // document.getElementById("googleMap").src = "https://www.google.com/maps/embed/v1/place?q=" + "locationName" + "&center=" + addLat + "+" + addLng + "&zoom=17&key=AIzaSyCHVB2GQRul-sTvAW8vzB0RFjJ5J7-90hY";
+// }
