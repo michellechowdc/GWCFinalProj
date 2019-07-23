5ec1af6ecf86409be554e4fe5cb2622744948073
Michelle Chow <40366174+michellechowdc@users.noreply.github.com>
2019-07-23 15:53:26 -0700

    Add files via upload

---
+++ about.js
@@ -0,0 +1,13 @@
+function openHome() {
+  document.location.href = "file:///C:/Users/GWC/Documents/GWC/FinalProject/Homepg/home.html";
+}
+function openMovies() {
+  document.location.href = "file:///C:/Users/GWC/Documents/GWC/FinalProject/Homepg/home.html#page-Movies";
+}
+// BOTH GO HOME
+function openReviews() {
+  document.location.href = "file:///C:/Users/GWC/Documents/GWC/FinalProject/Reviewpg/review.html";
+}
+function openAbout() {
+  document.location.href = "file:///C:/Users/GWC/Documents/GWC/FinalProject/Aboutpg/about.html";
+}
